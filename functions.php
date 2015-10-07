0eddc430d5af344c65c9b577dc8b97a1e59476aa
Harri Ruttas <harrrut@greeny.cs.tlu.ee>
2015-10-07 13:27:05 +0300

    6 tunni töö

--- functions.php
+++ functions.php
@@ -1,31 +1,44 @@
 <?php 
 	
-	require_once("../config_global.php");
-	$database = "if15_romil_3";
+	require_once("../../config_global.php");
+	$database = "if15_Harri_3";
+	
 	
 	
 	function getCarData(){
 		
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 
-		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color from car_plates");
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color from car_plates WHERE deleted IS NULL ");
 		$stmt->bind_result($id, $user_id_from_database, $number_plate, $color);
 		$stmt->execute();
 		
-		$row = 1;
+		// tekitan tühja massiivi, kus edaspidi hoian objekte
+		$car_array = array();
 		
+
 		//tee midagi seni, kuni saame ab'ist ühe rea andmeid
 		while($stmt->fetch()){
 			// seda siin sees tehakse 
 			// nii mitu korda kui on ridu
 
-			echo $row." ".$number_plate."<br>";
+			// tekitan objekti, kus hakkan hoidma väärtusi
+			$car = new StdClass();
+			$car->id = $id;
+			$car->plate = $number_plate;
+			$car->user_id = $user_id_from_database;
+			$car->color = $color;
 			
-			//$row = $row + 1;
-			//$row += 1;
-			$row++;
-			
+			//lisan massiivi ühe rea juurde
+			array_push($car_array, $car);
+			//var dump ütleb muutuja tüübi ja sisu
+			//echo "<pre>";
+			//var_dump($car_array);
+			//echo "</pre><br>";
 		}
+		
+		//tagastan massiivi, kus kõik read sees
+		return $car_array;
 		
 		
 		$stmt->close();
@@ -33,8 +46,26 @@
 	}
 	
 	
-	//käivitan funktsiooni
-	getCarData();
+	function deleteCar($id){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+
+		$stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id);
+		if($stmt->execute()){
+			// sai kustutatud
+			// kustutame aadressirea tühjaks
+			header("Location: table.php");
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		
+		
+	}
 	
+
 	
 ?>
